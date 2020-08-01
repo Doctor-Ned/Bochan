4f09a416649f95043781ce36c3537730851f8877
Doctor Ned <1933864+Doctor-Ned@users.noreply.github.com>
2020-08-01 12:51:24 +0200

    Corrected BOCHANAPI declarations in BochanAudioPlayer

--- BochanAudioLib/BochanAudioPlayer.h
+++ BochanAudioLib/BochanAudioPlayer.h
@@ -4,18 +4,18 @@
 #include "SDL.h"
 
 namespace bochan {
-    class BOCHANAPI BochanAudioPlayer sealed : public AudioPlayer{
+    class BochanAudioPlayer sealed : public AudioPlayer {
     public:
         BochanAudioPlayer() = default;
-        ~BochanAudioPlayer();
-        bool init(int sampleRate, size_t minBufferSize, size_t maxBufferSize) override;
-        void deinitialize() override;
-        bool isInitialized() const override;
-        size_t queueData(ByteBuffer* buff) override;
-        bool isPlaying() override;
-        void play() override;
-        void stop() override;
-        void flush() override;
+        BOCHANAPI ~BochanAudioPlayer();
+        BOCHANAPI bool init(int sampleRate, size_t minBufferSize, size_t maxBufferSize) override;
+        BOCHANAPI void deinitialize() override;
+        BOCHANAPI bool isInitialized() const override;
+        BOCHANAPI size_t queueData(ByteBuffer* buff) override;
+        BOCHANAPI bool isPlaying() override;
+        BOCHANAPI void play() override;
+        BOCHANAPI void stop() override;
+        BOCHANAPI void flush() override;
     private:
         static void fillData(void* ptr, Uint8* stream, int len);
         bool initialized{ false }, playing{ false };
